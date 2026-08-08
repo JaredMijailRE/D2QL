1b6f39b5e8c9f29538f61055a90b5800b0361dfa
kolmogorov <jramirezes@unal.edu.co>
2026-08-08 10:35:54 -0400

    prueba carga de librerias cloud sim

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from d2ql!")
+import random
+from pathlib import Path
+import jpype
+import jpype.imports
 
+libs_path = Path(__file__).parent / "libs" / "*"
 
-if __name__ == "__main__":
-    main()
+if not jpype.isJVMStarted():
+    jpype.startJVM(
+        "--enable-native-access=ALL-UNNAMED",
+        classpath=[str(libs_path)]
+    )
+
+from org.cloudsimplus.core import CloudSimPlus
+from org.cloudsimplus.datacenters import DatacenterSimple
+from org.cloudsimplus.hosts import HostSimple
+from org.cloudsimplus.resources import PeSimple
+from org.cloudsimplus.vms import VmSimple
+from org.cloudsimplus.cloudlets import CloudletSimple
+from org.cloudsimplus.brokers import DatacenterBrokerSimple
+from org.cloudsimplus.schedulers.cloudlet import CloudletSchedulerTimeShared
+from org.cloudsimplus.schedulers.vm import VmSchedulerTimeShared
+
+print("correcto")
